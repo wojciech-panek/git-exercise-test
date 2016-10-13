c18d9528f56832f007c71a8572a3b813f45e1f57
Wojciech Panek <wpanek@apptension.com>
2016-10-13 10:24:20 +0200

    Create newsletter component

---
+++ app/src/routes/home/newsletter/newsletter.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Newsletter extends Component {
+  render() {
+    return (
+      <div className="newsletter">
+        <input type="text"/>
+      </div>
+    );
+  }
+}
