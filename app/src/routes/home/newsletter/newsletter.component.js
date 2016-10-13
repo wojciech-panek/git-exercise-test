a0bb721c76b4e7bd61d6939d3a667e2a63ab7832
Wojciech Panek <wpanek@apptension.com>
2016-10-13 10:40:02 +0200

    Store newsletter email in component's state

--- app/src/routes/home/newsletter/newsletter.component.js
+++ app/src/routes/home/newsletter/newsletter.component.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 
 export class Newsletter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ''
+    };
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value})
+  }
+
   render() {
     return (
       <div className="newsletter">
-        <input type="text"/>
+        <input type="text" value={this.state.email} onChange={this.handleEmailChange.bind(this)}/>
       </div>
     );
   }
